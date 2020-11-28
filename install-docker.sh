1f948253f33cf8d57373de0d5e7725fa2a406899
cold-fronts <74230324+cold-fronts@users.noreply.github.com>
2020-11-28 19:33:06 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,10 +2,9 @@ echo HELLO \n start install docker!
 sudo apt install docker.io -y
 echo ################################
 echo docker install ok
-sudo usermod -aG dockerroot  ${USER}
-sudo usermod -aG docker ${USER}
-sudo chmod a+rw /var/run/docker.sock
-sudo systemctl restart docker
-docker info
+sudo groupadd docker 
+sudo gpasswd -a ${USER} docker
+newgrp docker
+docker ps
 echo ################################
 echo chmod ok
