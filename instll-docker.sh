a06e84dfa6b24450b625c57111ac69b8de7cd260
cold-fronts <74230324+cold-fronts@users.noreply.github.com>
2020-11-28 16:21:09 +0800

    Update instll-docker.sh

--- instll-docker.sh
+++ instll-docker.sh
@@ -2,7 +2,10 @@ echo HELLO \n start install docker!
 sudo apt install docker.io -y
 echo ################################
 echo docker install ok
+sudo usermod -aG dockerroot  ${USER}
+sudo usermod -aG docker ${USER}
 sudo chmod a+rw /var/run/docker.sock
+sudo systemctl restart docker
 docker info
 echo ################################
 echo chmod ok
