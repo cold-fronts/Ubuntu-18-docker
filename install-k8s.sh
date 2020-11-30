7990e731062967eb7e648685fb845d47e23dc0f0
cold-fronts <74230324+cold-fronts@users.noreply.github.com>
2020-11-30 09:41:09 +0800

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,8 +1,8 @@
 swapoff -a
 apt-get update && apt-get install -y apt-transport-https
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-EOF 
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list \n
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main \n
+EOF \n
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
